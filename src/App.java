1703a8b758ce22bceef604b64d6f3b18526b4425
unknown <iislamgom3a@gmail.com>
2024-12-08 22:44:12 +0200

    Jfram

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+
+public class App extends JFrame{
+    public static void main(String[] args) throws Exception {
+        GUIManager manager = new GUIManager();
+        manager.setVisible(true);
+    }
+}
---
+++ src/GUIManager.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+
+public class GUIManager extends JFrame{
+    public GUIManager(){
+        setSize(1000, 800);
+    }
+    
+}
