bcb0d375431a1e4b39f60fd9b9df465288b82abb
unknown <iislamgom3a@gmail.com>
2024-12-14 03:28:28 +0200

    GUI manager plan

--- src/main/java/GUIManager.java
+++ src/main/java/GUIManager.java
@@ -6,7 +6,20 @@ import java.awt.*;
 public class GUIManager extends JFrame{
     public GUIManager(){
         setSize(1000, 800);
-        setBackground(Color.BLACK);
+
     }
+    /*
+     - login panel or rejester if the user is not found
+
+     --- if the user exists: open a panel which the notes at the left and the edit panel at the right
+     - when clicking a note it will ask for a password
+     - passed: the notes will be shown at the right, and it can be changed from the editing mode to view and vice versa
+     - when a note closed it will be locked after a time
+     - a button to add new note and asking for its new password
+
+     --- user isn't exits : new user
+     - same but there is no old notes there
+
+    */
     
 }
